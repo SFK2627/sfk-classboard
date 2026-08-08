bea91e4aeabb2e5b7161471a304504b6eb8526ed
SFK <109145884+SFK2627@users.noreply.github.com>
2026-08-08 17:49:47 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sfk-main-pwa-v325-quote-no-sy-heart-bigger";
+const CACHE_NAME = "sfk-main-pwa-v326-quote-heart-lower-rounded-43";
 const CACHE_PREFIXES_TO_DELETE = ["sfk-main-pwa-", "sfk-sw.js-"];
 const NAVIGATION_FALLBACK_URL = "./index.html";
 const NAVIGATION_TIMEOUT_MS = 2500;
