871ad76c25ab9a37bf1e1e2d1d6d27ed7974ec46
SFK <109145884+SFK2627@users.noreply.github.com>
2026-08-11 14:32:11 +0800

    Add files via upload

--- pwa.js
+++ pwa.js
@@ -2,7 +2,7 @@
   let deferredInstallPrompt = null;
 
   const installButton = document.getElementById("installAppBtn");
-  const SW_URL = "./sw.js?v=buwan-wika-v437";
+  const SW_URL = "./sw.js?v=buwan-wika-v438";
   const CONTROLLER_RELOAD_KEY = "sfkPwaControllerReloadV427FloodSwimmerSync";
   const STANDALONE_BOOT_RELOAD_KEY = "sfkPwaStandaloneBootReloadV427FloodSwimmerSync";
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sfk-main-pwa-v437-buwan-wika";
+const CACHE_NAME = "sfk-main-pwa-v438-buwan-wika";
 const CACHE_PREFIXES_TO_DELETE = ["sfk-main-pwa-", "sfk-sw.js-"];
 const NAVIGATION_FALLBACK_URL = "./index.html";
 const NAVIGATION_TIMEOUT_MS = 2500;
