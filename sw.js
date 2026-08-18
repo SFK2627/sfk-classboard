e90306f285c44f1a6ce7f1f6d0a30853e07d5872
SFK <109145884+SFK2627@users.noreply.github.com>
2026-08-19 02:44:09 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sfk-main-pwa-v512-child-modal-close-safety";
+const CACHE_NAME = "sfk-main-pwa-v513-gif-tile-height-fix";
 const CACHE_PREFIXES_TO_DELETE = ["sfk-main-pwa-", "sfk-sw.js-"];
 const NAVIGATION_FALLBACK_URL = "./index.html";
 const NAVIGATION_TIMEOUT_MS = 2500;
